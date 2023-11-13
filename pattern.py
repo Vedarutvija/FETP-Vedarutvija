943d4f128acd92ed33ee153628fcd4196e0abe74
Vedarutvija <52282654+Vedarutvija@users.noreply.github.com>
2023-11-13 23:50:01 +0530

    minor edits in pattern loop

--- pattern.py
+++ pattern.py
@@ -1,15 +1,18 @@
 def generate_pattern(n):
     input_string = "FORMULAQSOLUTIONS"
     pattern_output = ""
+    last_row_chars = ""
 
     for i in range(n // 2 + 1):
         for space in range(n - i - 1):
             pattern_output += ""
         start_index = i
         end_index = i + (2 * i) + 1
+        row_chars = ""
         for char_index in range(start_index, end_index):
-            pattern_output += input_string[char_index % len(input_string)]
-        pattern_output += "\n"
+            row_chars += input_string[char_index % len(input_string)]
+        last_row_chars = row_chars
+        pattern_output += row_chars + "\n"
 
     for i in range(n // 2, 0, -1):
         for space in range(n - i):
@@ -18,7 +21,7 @@ def generate_pattern(n):
         start_index = n // 2 + 1 - i
         end_index = start_index + (2 * i) - 1
         for char_index in range(start_index, end_index):
-            pattern_output += input_string[char_index % len(input_string)]
+            pattern_output += last_row_chars[char_index % len(last_row_chars)]
 
         pattern_output += "\n"
 
